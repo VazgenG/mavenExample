c3df4d523efe91ec0256c66535b239110a85957e
Vazgen <vazgen04@gmail.com>
2022-01-17 19:18:57 +0400

    Initial commit

---
+++ src/main/java/StudentTest.java
@@ -0,0 +1,9 @@
+public class StudentTest {
+    public static void main(String[] args) {
+
+        Student student = new Student();
+        student.setName("Poxos");
+        System.out.println(student
+        );
+    }
+}
