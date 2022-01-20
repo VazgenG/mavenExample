4278518c5c20f6c41aaa1e5cecc8a05bd0735d50
Vazgen <vazgen04@gmail.com>
2022-01-20 20:35:48 +0400

    mavenExample

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,9 +1,13 @@
 package model;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Student {
 
